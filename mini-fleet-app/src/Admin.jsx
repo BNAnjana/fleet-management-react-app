da67a6db8948a6ca62ca8959f74cc594cf6546eb
BNAnjana <nagaanjana003@gmail.com>
2025-12-28 19:55:39 +0530

    second commit

---
+++ mini-fleet-app/src/Admin.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+function Admin() {
+    const [data, setData] = useState("");
+    const navigate = useNavigate();
+
+    function handleLogout() {
+        setData("");
+        navigate("/login");
+        alert("Logged out successfully");    
+    }
+    function handleAddFleet (e) {
+        e.preventDefault();
+        setData("");
+    }
+
+    return (
+        <>
+        <h2>Admin Page</h2>
+        <nav>
+            <ul>
+                <li><a href="/login" onClick={handleLogout}>Logout</a></li>
+            </ul>
+        </nav>
+        <aside>
+            <h3>Welcome to the Admin dashboard!</h3>
+            <p>Vehicle Reg No: </p><input type="text" name="vehicleRegNo"/>
+            <p>Category:</p> <select name="category" id="Category">
+                <option value="Auto">Auto</option>
+                <option value="Car">Car</option>
+                <option value="Truck">Truck</option>
+                <option value="Bus">Bus</option>
+            </select>
+            <p>Driver Name: </p><input type="text" name="driverName" />
+            <p>Availability Status:</p> <select name="availabilityStatus" id="AvailabilityStatus">
+                <option value="Available">Available</option>
+                <option value="Unavailable">Unavailable</option>
+            </select>
+            <br/>
+            <button type="submit" onClick={handleAddFleet}>Add Fleet</button>
+        </aside>
+        </>
+    )
+}
+export default Admin;
--- mini-fleet-app/src/Login.jsx
+++ mini-fleet-app/src/Login.jsx
@@ -1,28 +1,30 @@
-import {useState, useRef, useEffect, useNavigate} from "react";
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 
-function Login() {
-    const [email, setEmail] = useState("");
-    const [password, setpassword] = useState("");
-    const navigate = useNavigate();
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] =useState("");
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
 
-    const handleLogin = () => {
-        if(email === "admin@gmail.com" && password === "admin1234"){
-            alert("Login Success");
-            navigate("/admin");
-            }
-            else{
-                alert("Wrong email or password");
-            }
+  const handleLogin = () =>{
+    if(email === "admin@gmail.com" && password === "admin1234"){
+      localStorage.setItem("isLoggedIn","true");
+      navigate("/admin")
     }
-    return(
-        <div>
-            <h2>Login Page</h2>
-            <input type="email" placeholder="Enter your Email" value={email} onChange={(e) => setEmail(e.target.value)} />
-            <br />
-            <input type="password" placeholder="Enter your Password" value={password} onChange={(e) => setpassword(e.target.value)} />
-            <br />
-            <button onClick={handleLogin}>Login</button>
-        </div>
-    )
+    else{
+      setError("invalid email or password")
+    }
+  }
+  return (
+    <div style={{width: "40%", margin:"auto", marginTop: 100}}>
+        <h3>Login</h3>
+        <input type="email" placeholder='Enter email' value={email} onChange={(e) => setEmail(e.target.value)}/><br/>
+        <input type='password' placeholder='Enter password' value={password} onChange={(e) => setPassword(e.target.value)} /><br/>
+        <button onClick={handleLogin}>Login</button>
+        {error && <p style={{color: "red", fontSize: "20px"}}>{error}</p>}
+    </div>
+  )
 }
+
 export default Login;
